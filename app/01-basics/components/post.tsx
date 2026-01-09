3e1ab8a59910a251a63080241c50cae89bf325d7
David Bárcenas <dbarcenas.dev@gmail.com>
2026-01-08 21:15:14 -0600

    basicos: creación de componentes

---
+++ app/01-basics/components/post.tsx
@@ -0,0 +1,6 @@
+export default function Post() {
+    return <div>
+        <p>Davee</p>
+        <p>Nextjs refresh!</p>
+    </div>
+}
---
+++ app/01-basics/page.tsx
@@ -0,0 +1,10 @@
+import Post from "@/app/01-basics/components/post";
+
+export default function BasicsPage() {
+    return (
+        <section>
+            <h1 className="text-center text-2xl my-5">01 - Basics</h1>
+            <Post />
+        </section>
+    )
+}
